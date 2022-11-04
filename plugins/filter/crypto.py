6902bf13b9b3f3d8958603a9bd931da77d6f1435
Gauthier Leonard <gauthier@adaltas.com>
2022-11-04 20:22:54 +0100

    feat: add crypto conversion filters

---
+++ plugins/filter/crypto.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# Copyright 2022 TOSIT.IO
+# SPDX-License-Identifier: Apache-2.0
+
+from ansible.errors import AnsibleError
+
+import base58
+from web3 import Web3
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'cb58_to_hex': self.cb58_to_hex,
+            'cb58_to_bytes': self.cb58_to_bytes,
+            'hex_to_cb58': self.hex_to_cb58,
+            'hex_to_bytes': self.hex_to_bytes,
+        }
+
+    def cb58_to_hex(self, string):
+        return base58.b58decode(string).hex()
+
+    def cb58_to_bytes(self, string):
+        return base58.b58decode(string)
+
+    def hex_to_cb58(self, string):
+        return base58.b58encode(bytearray.fromhex(string))
+
+    def hex_to_bytes(self, string):
+        return Web3.toBytes(hexstr=string)

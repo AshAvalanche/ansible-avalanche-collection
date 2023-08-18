2b37c315c59183cf04d536144c92ee257446b13c
Gauthier Leonard <gauthier@e36knots.com>
2023-08-18 15:37:35 +0200

    feat(ash_cmd): secure private key with no_log

--- plugins/modules/ash_cmd.py
+++ plugins/modules/ash_cmd.py
@@ -12,9 +12,17 @@ import re
 
 from ansible.module_utils.basic import AnsibleModule
 
+FORBIDDEN_FLAGS = [
+    "config",
+    "json",
+    "private-key",
+    "help",
+    "version",
+]
+
 
 def run_module():
-    # define module arguments
+    # Define module arguments
     module_args = dict(
         command=dict(type="list", required=True),
         options=dict(type="dict", required=False, default={}),
@@ -26,55 +34,70 @@ def run_module():
             required=False,
             default="/etc/avalanche/ash-cli/conf/default.yml",
         ),
+        avalanche_private_key=dict(type="str", required=False, no_log=True),
         json=dict(type="bool", required=False, default=True),
     )
 
-    # define result argument
+    # Define result argument
     result = dict(changed=False)
 
-    # create the module instance
+    # Create the module instance
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
 
-    # stop here if there are flags/options in the command variable
-    # regex to match flags/options
+    # Fail if there are flags/options in the command variable
+    # Regex to match flags/options
     regex = r" --?\w+"
     if re.search(regex, " ".join(module.params["command"])):
         module.fail_json(
-            msg="The command parameter can not contain options or flags. Please use the options parameter.",
+            msg="The command parameter can not contain options or flags. Please use the 'options' parameter.",
             **result,
         )
 
-    # build the command
+    # Build the command
     command = [module.params["ash_path"]] + module.params["command"]
 
-    # add the params
+    # Add the params
     for key, value in module.params["options"].items():
-        # if value is boolean false, then skip
+        # If key is in FORBIDDEN_FLAGS, then fail
+        if key in FORBIDDEN_FLAGS:
+            module.fail_json(
+                msg="Passing '{}' as an option is not allowed. Please use the appropriate parameter.".format(
+                    key
+                ),
+                **result,
+            )
+        # If value is boolean false, then skip
         if value is False:
             continue
         command.append("--" + key)
-        # if value is boolean true, then it's a flag
+        # If value is boolean true, then it's a flag
         if value is True:
             continue
-        # if value is int, cast to string
+        # If value is int, cast to string
         if isinstance(value, int):
             value = str(value)
-        # if value is float, cast to string
+        # If value is float, cast to string
         if isinstance(value, float):
             value = str(value)
         command.append(value)
-    # force json output
+
+    # Add the private key
+    if module.params["avalanche_private_key"]:
+        command.append("--private-key")
+        command.append(module.params["avalanche_private_key"])
+
+    # Force json output
     if module.params["json"]:
         command.append("--json")
 
-    # add the config flag
+    # Add the config flag
     command.append("--config")
     command.append(module.params["ash_config"])
 
-    # run the command
+    # Run the command
     run = module.run_command(" ".join(command))
 
-    # exit with failure if rc is not 0
+    # Exit with failure if rc is not 0
     if run[0] > 0:
         module.fail_json(
             msg=run[2],
@@ -83,15 +106,15 @@ def run_module():
         )
 
     # TODO: act on result['changed'] when the cli will perform transactions
-    # result['changed'] = True
+    # Result['changed'] = True
 
-    # save the command that was executed
+    # Save the command that was executed
     result["command"] = " ".join(command)
 
-    # save the json output from stdout
+    # Save the json output from stdout
     result["output"] = json.loads(run[1])
 
-    # exit with the result if there was no error
+    # Exit with the result if there was no error
     module.exit_json(**result)
 
 
